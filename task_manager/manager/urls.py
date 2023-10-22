f6e5b865356a1389a6616a5386802bb054d61276
Dylan <dylan.okyere@gmail.com>
2023-10-22 14:24:35 +0200

    added some routes

--- task_manager/manager/urls.py
+++ task_manager/manager/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path
 from . import views 
 
-
+app_name = 'manager'
 urlpatterns = [
     path('', views.index, name='Home'),
+    path('tasklist/<int:task_list_id>', views.task_list, name='Task list'),
+    path('tasklist/task/<int:task_id>', views.task, name='Task'),
 ]
--- task_manager/manager/views.py
+++ task_manager/manager/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def index(request):
+    all_lists = models.TaskList.objects.all()
     context = {
-
+        'all_lists':all_lists
     }
     return render(request, 'manager/index.html', context)
+
+def task_list(request, task_list_id):
+    context = {
+        'task_list_id':task_list_id
+    }
+    return render(request, 'manager/task-list.html', context)
+
+
+def task(request, task_id):
+    context = {
+        'task_id':task_id
+    }
+    return render(request, 'manager/task.html', context)
